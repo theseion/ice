1e09d43253b5a11d3ece284d2cdd59cf141b5ad8
Esteban Lorenzano <estebanlm@gmail.com>
2015-08-22 18:21:30 +0200

    add travis info

---
+++ scripts/run-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+set -ex
+
+wget --quiet -O - get.pharo.org/50+vm | bash
+
+./pharo Pharo.image eval --save "
+Metacello new 
+	baseline: 'Ice';
+	repository: 'filetree://./mc';
+	load.
+"
+
+./pharo Pharo.image test --fail-on-failure "Ice.*"
