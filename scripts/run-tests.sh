bf5c061b34e6150989cbc75f5d83ffcfb95e226c
Esteban Lorenzano <estebanlm@gmail.com>
2015-09-02 12:40:42 +0200

    redirect stderr to stdout

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -7,8 +7,8 @@ wget --quiet -O - get.pharo.org/50+vm | bash
 ./pharo Pharo.image eval --save "
 Metacello new 
 	baseline: 'Ice';
-	repository: 'filetree://./mc';
+	repository: 'filetree://mc';
 	load.
 "
 
-./pharo Pharo.image test --fail-on-failure "Ice.*"
+./pharo Pharo.image test --fail-on-failure "Ice.*" 2>&1
